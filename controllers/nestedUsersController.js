c394dddd1e4a923917ea74458f9e33d458234e0f
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-24 15:54:35 +0100

    delete works

--- controllers/nestedUsersController.js
+++ controllers/nestedUsersController.js
@@ -1,35 +1,34 @@
-    import NestedUsersModel from '../models/nestedUsersModel.js';
-     
-    export const getAllNestedUsers = async () => {
-        return await NestedUsersModel.find({});
-    }
-     
-    export const getAllNestedUsersEmails = async () => {
-        return await NestedUsersModel.find({}, {email: 1});
-    }
-     
-    export const getAccountHistoryOfNestedUser = async (id) => {
-        return await NestedUsersModel.findById(
-            id, {
-            accountHistory: 1
-        });
-    }
-     
-    export const createNestedUser = async (nestedUser, callback) => {
-        const nestedUserModel = new NestedUsersModel(nestedUser);
-        const result = nestedUserModel.save((err, docs) => {
-            if (err) {
-                callback(err);
-            } else {
-                callback(docs);
-            }
-        });
-    }
-     
-    export const deleteNestedUser = async (id) => {
-        return await NestedUsersModel.findByIdAndRemove(id);
-    }
-     
-    export const updateNestedUser = async (id, updateFields) => {
-        return await NestedUsersModel.findByIdAndUpdate(id, updateFields, { new: true });
-    }
+import NestedUsersModel from '../models/nestedUsersModel.js';
+
+export const getAllNestedUsers = async () => {
+	return await NestedUsersModel.find({});
+}
+
+export const getAllNestedUsersEmails = async () => {
+	return await NestedUsersModel.find({}, { email: 1 });
+}
+
+export const getAccountHistoryOfNestedUser = async (id) => {
+	return await NestedUsersModel.findById(
+		id, {
+		accountHistory: 1
+	});
+}
+
+export const createNestedUser = async (nestedUser, callback) => {
+	await NestedUsersModel.create(nestedUser, (err, docs) => {
+		if (err) {
+			callback(err);
+		} else {
+			callback(docs);
+		}
+	});
+}
+
+export const deleteNestedUser = async (id) => {
+	return await NestedUsersModel.findByIdAndRemove(id);
+}
+
+export const updateNestedUser = async (id, updateFields) => {
+	return await NestedUsersModel.findByIdAndUpdate(id, updateFields, { new: true });
+}
